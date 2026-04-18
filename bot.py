f1088db9ab70437d4ff8ed99603d5aa8bec10380
E-Aleksandr <git.AleksandraE@gmail.com>
2026-04-18 20:32:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,32 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiogram.filters import Command
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-WEBAPP_URL = "https://compassionate-amazement-production-b6b2.up.railway.app"
+WEBAPP_URL = os.environ.get("URL1")
+WEBAPP_URL2 = os.environ.get("URL2")
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(os.environ.get("BOT_TOKEN"))
 dp = Dispatcher()
 
+msg = """
+В ДВА СТВОЛА
+
+Турнир в новом формате с улучшенным балансом. Теперь одержать победу сможет каждый, не зависимо от статы и скилла!
+
+Участники делятся на пары, затем составляется турнирная сетка. Происходит PvP сражение между командами, победитель переходит в следующий раунд. Лучший дуэт побеждает в турнире.
+"""
 @dp.message(Command("start"))
 async def start_cmd(message: types.Message):
+    await message.answer("Бот готов!")
+
+@dp.message(Command("post"))
+async def start_cmd(message: types.Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="🎮 Играть", web_app=WebAppInfo(url=WEBAPP_URL))]
+        [InlineKeyboardButton(text="Подробности", web_app=WebAppInfo(url=WEBAPP_URL))]
     ])
-    await message.answer("Нажми на кнопку, чтобы открыть игру.", reply_markup=keyboard)
+    keyboard2 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Прогнозы", web_app=WebAppInfo(url=WEBAPP_URL2))]
+    ])
+    await message.answer(msg, reply_markup=keyboard, reply_markup=keyboard2)
 
 async def main():
     print("✅ Бот запущен!")
